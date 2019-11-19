0c056fed1b12468d80adae8e4c7d76a1e30edf3f
gnardinosaur <github email address>
2019-11-19 12:37:36 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let timeArr = time.split(":");
+  let num = parseInt(timeArr[0]);
+
+  if (num < 12) {
+    return "Good Morning"
+  } else if (num > 12 && num < 17) {
+    return "Good Afternoon"
+  } else if (num > 17) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  let greetingNode = document.getElementById('greeting');
+  greetingNode.innerText = str;
+}
